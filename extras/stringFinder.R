16d4da28ef6ce2fa56559bab885f48b7ec1ad1bf
chandryou <applegna@gmail.com>
2019-11-26 12:57:18 +0900

    add String Finder script

---
+++ extras/stringFinder.R
@@ -0,0 +1,13 @@
+stringOfInterest<-"methodEval"
+
+rmdFiles<-list.files()[grepl(".Rmd",list.files())]
+
+#identify where the string of interest exists.
+for (singleRmd in rmdFiles){
+  #print(singleRmd)
+  if(length(grep(stringOfInterest, readLines(singleRmd), value = TRUE))){
+    print(singleRmd)
+    grep(stringOfInterest, readLines(singleRmd), value = TRUE)
+  }
+}
+
